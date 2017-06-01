2d3254df40ddea72b1f03969a0cdb9094225df26
Keith Williams <kwilliams@salsify.com>
2017-06-01 11:32:02 -0400

    not-test-output

--- testem.js
+++ testem.js
@@ -2,7 +2,7 @@
 module.exports = {
   "test_page": "tests/index.html?hidepassed",
   "disable_watching": true,
-  "report_file": `./tmp/test-output/${process.env.EMBER_TRY_CURRENT_SCENARIO}/output.xml`,
+  "report_file": `./tmp/not-test-output/${process.env.EMBER_TRY_CURRENT_SCENARIO}/output.xml`,
   "launch_in_ci": [
     "Chrome"
   ],
